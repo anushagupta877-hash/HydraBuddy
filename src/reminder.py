c06cc5340e55282b70338824e4e58249445df1df
Aanya Verma <averma1_be25@thapar.edu>
2026-07-11 12:24:35 +0530

    Implemented reminder timer

---
+++ src/reminder.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+class Reminder:
+    def __init__(self, interval_minutes=30):
+        self.interval = interval_minutes * 60
+        self.running = False
+
+    def start(self):
+        self.running = True
+        threading.Thread(target=self._run, daemon=True).start()
+
+    def stop(self):
+        self.running = False
+
+    def _run(self):
+        while self.running:
+            time.sleep(self.interval)
+            print("💧 Time to drink water!")
